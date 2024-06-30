3e13b21acd4e6f82619950f1d18f1db7cd505433
cocacola-lover <pegas620570@gmail.com>
2024-06-30 13:42:47 +0300

    Refactor main.c

--- main.c
+++ main.c
@@ -2,33 +2,41 @@
 #include <SDL2/SDL.h>
 #include "headers/agent_field.h"
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 const int NUMBER_OF_AGENTS = 10;
+const int MAX_ITERATION = 1000;
+const int ITERATION_EVERY = 10;
+
+const unsigned int RANDOM_SEED = 0;
 
 int main(void)
 {
-    srand(0);
+    srand(RANDOM_SEED);
 
-    char **traceMap = mallocTraceMap(640, 480);
+    char **traceMap = mallocTraceMap(SCREEN_WIDTH, SCREEN_HEIGHT);
     struct Agent *agentArray = mallocAgents(NUMBER_OF_AGENTS);
 
-    SDL_Window *window;     // Declare a window
-    SDL_Renderer *renderer; // Declare a renderer
+    SDL_Window *window;
+    SDL_Renderer *renderer;
 
-    SDL_Init(SDL_INIT_VIDEO); // Initialize SDL2
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow("Game Window",           // window title
-                              SDL_WINDOWPOS_UNDEFINED, // initial x position
-                              SDL_WINDOWPOS_UNDEFINED, // initial y position
-                              640,                     // width, in pixels
-                              480,                     // height, in pixels
-                              0                        // flags
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow(
+        "Slime Simulation",
+        SDL_WINDOWPOS_UNDEFINED, // initial x position
+        SDL_WINDOWPOS_UNDEFINED, // initial y position
+        SCREEN_WIDTH,            // width, in pixels
+        SCREEN_HEIGHT,           // height, in pixels
+        0                        // flags
     );
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < MAX_ITERATION; i++)
     {
 
-        iterateAgents(agentArray, NUMBER_OF_AGENTS, traceMap, 640, 480);
+        iterateAgents(agentArray, NUMBER_OF_AGENTS, traceMap, SCREEN_WIDTH, SCREEN_HEIGHT);
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
@@ -36,9 +44,9 @@ int main(void)
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-        for (int j = 0; j < 640; j++)
+        for (int j = 0; j < SCREEN_WIDTH; j++)
         {
-            for (int k = 0; k < 480; k++)
+            for (int k = 0; k < SCREEN_HEIGHT; k++)
             {
                 char alpha = traceMap[j][k];
                 SDL_SetRenderDrawColor(renderer, alpha, alpha, alpha, 255);
@@ -48,10 +56,10 @@ int main(void)
 
         SDL_RenderPresent(renderer);
 
-        SDL_Delay(100);
+        SDL_Delay(ITERATION_EVERY);
     }
 
-    freeTraceMap(traceMap, 640);
+    freeTraceMap(traceMap, SCREEN_WIDTH);
     freeAgents(agentArray);
 
     // Close and destroy the window
